bcf32262b76a13fa22e83a9ca4fa09ed03256fb8
atharavmishra <atharvmishra77@gmail.com>
2024-08-19 12:37:16 +0530

    i

---
+++ app/src/main/java/com/example/instagramclone/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.instagramclone
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.instagramclone.ui.theme.InstagramCloneTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            ProfileScreen()
+        }
+
+    }
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    InstagramCloneTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/instagramclone/ProfileScreen.kt
@@ -0,0 +1,70 @@
+package com.example.instagramclone
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ProfileScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        TopBar(name = "Atharv Mishra")
+        Spacer(modifier = Modifier.height(4.dp))
+    }
+}
+
+@Composable
+fun TopBar(
+    name: String, modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(top = 15.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        Icon(
+            imageVector = Icons.Default.ArrowBack,
+            contentDescription = "Back Arrow",
+            tint = Color.Black,
+            modifier = Modifier.size(24.dp)
+        )
+        Text(
+            text = name,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+        Icon(
+            painter = painterResource(id = R.drawable.ic_bell),
+            contentDescription = "Back Arrow",
+            tint = Color.Black,
+            modifier = Modifier.size(24.dp)
+        )
+        Icon(
+            painter = painterResource(id = R.drawable.ic_dotmenu),
+            contentDescription = "Back Arrow",
+            tint = Color.Black,
+            modifier = Modifier.size(24.dp)
+        )
+    }
+}
